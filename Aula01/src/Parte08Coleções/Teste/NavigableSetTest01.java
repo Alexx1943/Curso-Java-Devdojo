8c35dd14de4477fb6442be561d5c49b6bcef2603
Alex <alextavares1943@gmail.com>
2025-02-25 17:01:47 -0300

    Set

--- Aula01/src/Parte08Coleções/Teste/NavigableSetTest01.java
+++ Aula01/src/Parte08Coleções/Teste/NavigableSetTest01.java
@@ -14,6 +14,14 @@ class SmarthphoneComperetor implements Comparator<Smartphone> {
         return o1.getMarca().compareTo(o2.getMarca());
     }
 }
+class MangaPrecoComparator implements Comparator<Manga>{
+    @Override
+    public int compare(Manga o1, Manga o2) {
+        return Double.compare(o1.getPreco(), o2.getPreco());
+    }
+}
+
+
 
 public class NavigableSetTest01 {
     public static void main(String[] args) {
@@ -22,7 +30,7 @@ public class NavigableSetTest01 {
         Smartphone smartphone = new Smartphone("1234", "Iphone");
         set.add(smartphone);
 
-        NavigableSet<Manga> mangas = new TreeSet<>(); // Ele organiza de acordo com o comparetor da classe de referencia.
+        NavigableSet<Manga> mangas = new TreeSet<>(new MangaPrecoComparator()); // Ele organiza de acordo com o comparetor da classe de referencia.
         mangas.add(new Manga(3L, "Pokemon", 2.99, 1));
         mangas.add(new Manga(5L, "Helsing ultimete", 5.00, 2));
         mangas.add(new Manga(4L, "Dragon ball Z", 0.99, 1));
@@ -30,9 +38,29 @@ public class NavigableSetTest01 {
         mangas.add(new Manga(1L, "Attack on titan", 9.99, 5));
         mangas.add(new Manga(10L, "Aaregon", 9.99, 5));
 
-        for (Manga manga:mangas){
+        for (Manga manga:mangas) { //descendingSet() = Inverte a ordem
             System.out.println(manga);
         }
+Manga yuyu = new Manga(11L, "yuyu hakusho", 2.99,5);
+// lower < Chama o de menor valor.
+// floor <= Chama o quem tem o mesmo valor.
+// higer > Chama o de maior valor.
+// ceilinng >= leva em consideração o valor que foi passado.
+
+        System.out.println("-------------------------------------------------------------");
+
+        System.out.println(mangas.lower(yuyu));
+        System.out.println(mangas.floor(yuyu));
+        System.out.println(mangas.higher(yuyu));
+        System.out.println(mangas.ceiling(yuyu));
+
+        System.out.println("-------------------------------------------------------------");
+
+        System.out.println(mangas.size());
+        System.out.println(mangas.pollFirst()); // Lista e remove o primeiroobjeto da lista.
+        System.out.println(mangas.pollLast()); // Lista e remove o ultimo objeto da lista.
+        System.out.println(mangas.size());
+
 
 
     }
