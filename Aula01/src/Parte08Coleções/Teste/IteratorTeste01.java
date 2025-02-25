6a70514d0bc2166f3ed78b9d81779cbe51babf91
Alex <alextavares1943@gmail.com>
2025-02-25 13:59:23 -0300

    Curso Java Devdojo.1

--- Aula01/src/Parte08Coleções/Teste/IteratorTeste01.java
+++ Aula01/src/Parte08Coleções/Teste/IteratorTeste01.java
@@ -11,11 +11,11 @@ public class IteratorTeste01 {
     public static void main(String[] args) {
 
         List<Manga> mangas = new ArrayList<>(6);
-        mangas.add(new Manga(4L, "Dragon ball Z", 0.99, 0));
-        mangas.add(new Manga(1L, "Attack on titan", 9.99, 5));
-        mangas.add(new Manga(2L, "Berserk", 7.25, 0));
         mangas.add(new Manga(3L, "Pokemon", 2.99, 1));
         mangas.add(new Manga(5L, "Helsing ultimete", 5.00, 2));
+        mangas.add(new Manga(4L, "Dragon ball Z", 0.99, 1));
+        mangas.add(new Manga(2L, "Berserk", 7.25, 0));
+        mangas.add(new Manga(1L, "Attack on titan", 9.99, 5));
 
         Iterator<Manga> mangaIterator = mangas.iterator();
         mangas.removeIf(manga -> manga.getQuantidade() == 0); // Alternativa mais viavel.
