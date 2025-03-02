d6e0a953ced8eac8b752c643afc54eeeba9a5199
Alex <alextavares1943@gmail.com>
2025-03-02 15:00:25 -0300

    Classe-Internas

--- Aula01/src/Parte10ClassesInternas/Test/OuterClassesTest02.java
+++ Aula01/src/Parte10ClassesInternas/Test/OuterClassesTest02.java
@@ -1,7 +1,24 @@
 package Parte10ClassesInternas.Test;
 
 public class OuterClassesTest02 {
+    private String name = "Alex";
+
+    void print() {
+        class LocalClass {
+            final private String lastName = "Tavares";
+
+            public void printLocal() {
+                System.out.println(name + " " + lastName);
+            }
+        }
+//        LocalClass localClass = new LocalClass(); outra forma possivel
+//        localClass.printLocal();
+        new LocalClass().printLocal();
+    }
+
     public static void main(String[] args) {
 
+        OuterClassesTest02 outerClass = new OuterClassesTest02();
+        outerClass.print();
     }
 }
