14a38b28f6b31a2627642cdb0fc3983ae65da116
Alex <alextavares1943@gmail.com>
2025-03-03 16:48:12 -0300

    Lambdas-Consumer

--- Aula01/src/Parte12Lambdas/Test/LambdasTest01.java
+++ Aula01/src/Parte12Lambdas/Test/LambdasTest01.java
@@ -1,9 +1,26 @@
 package Parte12Lambdas.Test;
 
-// Interface funcional é uma interface que tem um unico metodo abstrato.
-// A ideia por trás das interfaces funcionais é permitir que elas possam ser utilizadas como expressões lambda.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class LambdasTest01 {
     public static void main(String[] args) {
+
+        List<String> nomes = List.of("Alex", "Allan");
+        List<Integer> numeros = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Character> chars = List.of('A', 'B', 'C');
+
+        forEach(numeros, numero -> System.out.println(numero));
+        forEach(nomes, nome -> System.out.println(nome));
+        forEach(chars, character -> System.out.println(character));
+
+    }
+
+    private static <T> void forEach(List<T> t, Consumer<T> consumer) {
+        for (T e : t) {
+            consumer.accept(e);
+        }
 
     }
 }
