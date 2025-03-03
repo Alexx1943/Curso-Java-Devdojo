1e60ed7a8584de5376bb3050716f36a61d0fe0b3
Alex <alextavares1943@gmail.com>
2025-03-03 14:40:29 -0300

    Lambdas

---
+++ Aula01/src/Parte12Lambdas/Test/LambdasTest01.java
@@ -0,0 +1,9 @@
+package Parte12Lambdas.Test;
+
+// Interface funcional é uma interface que tem um unico metodo abstrato.
+// A ideia por trás das interfaces funcionais é permitir que elas possam ser utilizadas como expressões lambda.
+public class LambdasTest01 {
+    public static void main(String[] args) {
+
+    }
+}
