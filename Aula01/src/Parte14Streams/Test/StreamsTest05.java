634964bd9d8f8c0d44558aa42e1e090f64732192
Alex <alextavares1943@gmail.com>
2025-03-08 09:59:15 -0300

    Streams

--- Aula01/src/Parte14Streams/Test/StreamsTest05.java
+++ Aula01/src/Parte14Streams/Test/StreamsTest05.java
@@ -1,12 +1,11 @@
 package Parte14Streams.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class StreamsTes05 {
+public class StreamsTest05 {
     public static void main(String[] args) {
 
         List<String> words = List.of("Gomu", "Gomu", "No", "Mi");
@@ -14,8 +13,10 @@ public class StreamsTes05 {
         System.out.println(Arrays.toString(lettler));
 
         List<String[]> collect = words.stream().map(w -> w.split("")).collect(Collectors.toList());
+        System.out.println(collect);
 
         Stream<String> stream = Arrays.stream(lettler);
+        System.out.println(stream);
 
         List<String> lettler1 = words.stream()
                 .map(w -> w.split(""))
