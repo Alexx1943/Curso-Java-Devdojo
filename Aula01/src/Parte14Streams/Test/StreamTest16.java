72a316e5846df63c7f40ba2dc7cc0df9a5721ffb
Alex <alextavares1943@gmail.com>
2025-03-14 20:21:55 -0300

    Stream

--- Aula01/src/Parte14Streams/Test/StreamTest16.java
+++ Aula01/src/Parte14Streams/Test/StreamTest16.java
@@ -1,24 +1,69 @@
 package Parte14Streams.Test;
 
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
 public class StreamTest16 {
     public static void main(String[] args) {
+
+
         System.out.println(Runtime.getRuntime().availableProcessors());
 
-        long num = 10_000_000;
+        long num = 50_000_000;
+
         sumfor(num);
+        sumStreamInterate(num);
+        sumParallelStreamInterate(num);
+        sumLongStreamInterate(num);
+        sumParallelLongStreamInterate(num);
 
 
     }
 
     private static void sumfor(long num) {
-        System.out.println("sun for");
+        System.out.print("sun for: ");
         long result = 0;
         long unit = System.currentTimeMillis();
+
         for (long i = 1; i <= num; i++) {
             result += i;
         }
         long end = System.currentTimeMillis();
-        System.out.println(result + " " +(end-unit)+"ms");
+        System.out.println(result + " " + (end - unit) + "ms");
     }
+
+    private static void sumStreamInterate(long num) {
+        System.out.print("sun StreamInterate: ");
+        long unit = System.currentTimeMillis();
+        long result = Stream.iterate(1L, i -> i + 1).limit(num).reduce(0L, Long::sum);
+        long end = System.currentTimeMillis();
+        System.out.println(result + " " + (end - unit) + "ms");
+    }
+
+    private static void sumParallelStreamInterate(long num) {
+        System.out.print("sun ParallelStreamInterate: ");
+        long unit = System.currentTimeMillis();
+        long result = Stream.iterate(1L, i -> i + 1).limit(num).parallel().reduce(0L, Long::sum);
+        long end = System.currentTimeMillis();
+        System.out.println(result + " " + (end - unit) + "ms");
+    }
+
+    private static void sumLongStreamInterate(long num) {
+        System.out.print("sun sumLongStreamInterate: ");
+        long unit = System.currentTimeMillis();
+        long result = LongStream.rangeClosed(1L, num).reduce(0L, Long::sum);
+        long end = System.currentTimeMillis();
+        System.out.println(result + " " + (end - unit) + "ms");
+    }
+
+    private static void sumParallelLongStreamInterate(long num) {
+        System.out.print("sun sumParallelLongStreamInterate: ");
+        long unit = System.currentTimeMillis();
+        long result = LongStream.rangeClosed(1L, num).parallel().reduce(0L, Long::sum);
+        long end = System.currentTimeMillis();
+        System.out.println(result + " " + (end - unit) + "ms");
+    }
+
+
 
 }
