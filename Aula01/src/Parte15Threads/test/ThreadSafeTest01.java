813da69944ef20de8d9c88498351fdfc099a7375
Alex <alextavares1943@gmail.com>
2025-03-17 15:50:38 -0300

    Threads

--- Aula01/src/Parte15Threads/test/ThreadSafeTest01.java
+++ Aula01/src/Parte15Threads/test/ThreadSafeTest01.java
@@ -1,7 +1,35 @@
 package Parte15Threads.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+class ThredSafeNames{
+    List<String> names = new ArrayList<>();
+
+    public synchronized void add(String name){
+        names.add(name);
+    }
+
+    public synchronized void removeFirst(){
+        if (names.isEmpty()) {
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(names.remove(0));
+        }
+    }
+
+
+}
 public class ThreadSafeTest01 {
     public static void main(String[] args) {
+
+        ThredSafeNames thredSafeNames = new ThredSafeNames();
+        thredSafeNames.add("Alex");
+
+        Runnable run = thredSafeNames::removeFirst;
+
+        new Thread(run).start();
+        new Thread(run).start();
 
 
         
