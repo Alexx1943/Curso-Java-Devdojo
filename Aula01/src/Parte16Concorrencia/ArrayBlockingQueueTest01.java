bdb9674f1d907dbabc4e5ae8e5b897e630356d8a
Alex <alextavares1943@gmail.com>
2025-03-31 21:42:53 -0300

    Concorrencia

--- Aula01/src/Parte16Concorrencia/ArrayBlockingQueueTest01.java
+++ Aula01/src/Parte16Concorrencia/ArrayBlockingQueueTest01.java
@@ -1,7 +1,40 @@
 package Parte16Concorrencia;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 public class ArrayBlockingQueueTest01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        BlockingQueue<String> bq = new ArrayBlockingQueue<>(1);
+
+        bq.put("Alex");
+        System.out.printf("%s Added the value $s%n", Thread.currentThread().getName(), bq.peek());
+        System.out.println("Trying to add onether value ");
+        bq.put("Tavares");
+        System.out.printf("%s Added the value $s%n", Thread.currentThread().getName(), bq.peek());
+
+    }
+
+    static class removeFromQueue implements Runnable {
+
+        BlockingQueue<String> bq = new ArrayBlockingQueue<>(1);
+
+        public removeFromQueue(BlockingQueue<String> bq) {
+            this.bq = bq;
+        }
+
+        @Override
+        public void run() {
+            System.out.printf("%s Going to sleep for 2s  $s%n", Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(2);
+                System.out.printf("%s Removing value from queue $s%n", Thread.currentThread().getName(), bq.take());
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
---
+++ Aula01/src/Parte16Concorrencia/Test/ExecutorsTest01.java
@@ -0,0 +1,46 @@
+package Parte16Concorrencia.Test;
+
+import Parte06OrientacaoObjetos.GAssocição.dominio.Time;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Printer implements Runnable{
+    private final int num;
+
+    public Printer(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("%s Iniciou:%d%n", Thread.currentThread().getName(), num);
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("%s Finalizou:%d%n", Thread.currentThread().getName(),num);
+    }
+}
+public class ExecutorsTest01 {
+    public static void main(String[] args) {
+
+//      ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()); // Utilizando todas as threads do meu processador.
+//      ExecutorService executorService = Executors.newSingleThreadExecutor(); // Utilizando apenas uma thread.
+        ExecutorService executorService = Executors.newCachedThreadPool(); // Cria thread de acordo com a necessidade
+        executorService.execute(new Printer(1));
+        executorService.execute(new Printer(2));
+        executorService.execute(new Printer(3));
+        executorService.execute(new Printer(4));
+        executorService.execute(new Printer(5));
+        executorService.execute(new Printer(6));
+        executorService.execute(new Printer(7));
+        executorService.execute(new Printer(8));
+        executorService.execute(new Printer(9));
+        executorService.execute(new Printer(10));
+       executorService.shutdown();
+
+    }
+}
