73d3e080d14d33795344466a53486780c8e27346
Alex <alextavares1943@gmail.com>
2025-04-03 22:52:17 -0300

    Concorrencia

---
+++ Aula01/src/Parte16Concorrencia/Service/StoreService.java
@@ -0,0 +1,44 @@
+package Parte16Concorrencia.Service;
+
+
+import java.util.concurrent.*;
+
+public class StoreService {
+
+    private static final ExecutorService executor = Executors.newCachedThreadPool(); // Podendo criar threads de acordo com a necessecidade.
+
+    public double getPriceSync(String storeName){
+        System.out.printf("Getting prices sync fo store %s%n",storeName);
+        return priceGenerator();
+    }
+
+    public Future<Double> getPricesAsSyncFuture(String storeName){
+        System.out.printf("Getting prices sync fo store %s%n",storeName);
+        return executor.submit(this::priceGenerator);
+    }
+
+    public CompletableFuture<Double> getPricesAsSyncCompletableFuture(String storeName){
+        System.out.printf("Getting prices sync fo store %s%n",storeName);
+        return CompletableFuture.supplyAsync(this::priceGenerator);
+    }
+
+    public void shutdown(){
+        executor.shutdown();
+    }
+
+    private double priceGenerator(){
+        System.out.printf("%s generating price: %n", Thread.currentThread().getName());
+        delay();
+        return ThreadLocalRandom.current().nextInt(1,500)*37;
+    }
+
+    private void delay(){
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
--- Aula01/src/Parte16Concorrencia/Test/CompletableFutureTest01.java
+++ Aula01/src/Parte16Concorrencia/Test/CompletableFutureTest01.java
@@ -1,4 +1,67 @@
 package Parte16Concorrencia.Test;
 
+import Parte16Concorrencia.Service.StoreService;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 public class CompletableFutureTest01 {
+    public static void main(String[] args) {
+
+        StoreService storeService = new StoreService();
+        searchPricesSync(storeService);
+        System.out.println("-------------------------------------------");
+        searchPricesAsSyncSFuture(storeService);
+        System.out.println("-------------------------------------------");
+        searchPricesAsSyncCompletableFuturee(storeService);
+    }
+
+    private static void searchPricesSync(StoreService storeService) {
+        long start = System.currentTimeMillis();
+        System.out.println(storeService.getPriceSync("Store 1"));
+        System.out.println(storeService.getPriceSync("Store 2"));
+        System.out.println(storeService.getPriceSync("Store 3"));
+        System.out.println(storeService.getPriceSync("Store 4"));
+        long end = System.currentTimeMillis();
+        System.out.printf("Time passed to searchPricesSync %dms%n", (end - start));
+    }
+
+    private static void searchPricesAsSyncSFuture(StoreService storeService) {
+        long start = System.currentTimeMillis();
+        Future<Double> pricesAsSyncSFuture1 = storeService.getPricesAsSyncFuture("Store 1");
+        Future<Double> pricesAsSyncSFuture2 = storeService.getPricesAsSyncFuture("Store 2");
+        Future<Double> pricesAsSyncSFuture3 = storeService.getPricesAsSyncFuture("Store 3");
+        Future<Double> pricesAsSyncSFuture4 = storeService.getPricesAsSyncFuture("Store 4");
+        try {
+            System.out.println(pricesAsSyncSFuture1.get());
+            System.out.println(pricesAsSyncSFuture2.get());
+            System.out.println(pricesAsSyncSFuture3.get());
+            System.out.println(pricesAsSyncSFuture4.get());
+        } catch (InterruptedException | ExecutionException e) {
+
+            throw new RuntimeException(e);
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.printf("Time passed to searchPricesSync %dms%n", (end - start));
+        storeService.shutdown();
+    }
+
+    private static void searchPricesAsSyncCompletableFuturee(StoreService storeService) {
+        long start = System.currentTimeMillis();
+        CompletableFuture<Double> pricesAsSyncSFuture1 = storeService.getPricesAsSyncCompletableFuture("Store 1");
+        CompletableFuture<Double> pricesAsSyncSFuture2 = storeService.getPricesAsSyncCompletableFuture("Store 2");
+        CompletableFuture<Double> pricesAsSyncSFuture3 = storeService.getPricesAsSyncCompletableFuture("Store 3");
+        CompletableFuture<Double> pricesAsSyncSFuture4 = storeService.getPricesAsSyncCompletableFuture("Store 4");
+
+            System.out.println(pricesAsSyncSFuture1.join());
+            System.out.println(pricesAsSyncSFuture2.join());
+            System.out.println(pricesAsSyncSFuture3.join());
+            System.out.println(pricesAsSyncSFuture4.join());
+
+        long end = System.currentTimeMillis();
+        System.out.printf("Time passed to searchPricesSync %dms%n", (end - start));
+
+    }
 }
